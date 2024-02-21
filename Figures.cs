6417fc693e66f54fd754b3a86570c78fd3b7f79e
PavelVasilchikov <91344984+PavelVasilchikov@users.noreply.github.com>
2024-02-21 14:44:03 +0300

    Добавьте файлы проекта.

---
+++ Figures.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LibraryFigures
+{
+    
+    public interface IFigure
+    {   
+        float FindArea();
+    }
+
+    public class Circle:IFigure
+    {
+        float _rad;
+        const float pi = 3.14f;
+ 
+        public Circle(float rad)
+        {
+            _rad = rad;
+        }
+        
+        public float FindArea()
+        { 
+            return _rad*_rad*pi;
+        }
+    }
+
+    public class Triangle:IFigure
+    {
+        float _sideA;
+        float _sideB;
+        float _sideC;
+
+        public Triangle(float sideA, float sideB, float sideC)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+        }
+
+        public bool CheckTriangle()
+        {
+            bool check1 = Math.Pow(_sideA, 2) == Math.Pow(_sideB, 2) + Math.Pow(_sideC, 2);
+            bool check2 = Math.Pow(_sideB, 2) == Math.Pow(_sideA, 2) + Math.Pow(_sideC, 2);
+            bool check3 = (Math.Pow(_sideC, 2) == Math.Pow(_sideB, 2) + Math.Pow(_sideA, 2));
+
+            return check1 || check2 || check3;
+        }
+
+        public float FindArea()
+        {
+            float halfMeter = (_sideA + _sideB + _sideC) / 2;
+            return (float)Math.Sqrt(halfMeter*(halfMeter - _sideA)*(halfMeter - _sideB)*(halfMeter - _sideC));
+        }
+
+    }
+
+    public static class IsRectangular
+    {
+        public static bool Triangle(this IFigure figure)
+        {
+            if (figure is Triangle triangle)
+            {
+                return triangle.CheckTriangle();
+            }
+
+            return false;
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using LibraryFigures;
+using System;
+
+namespace Test
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            float radius = 2.4f;
+
+            float Aside = 6.0f;
+            float Bside = 8.4f;
+            float Cside = 10.2f;
+
+            IFigure circle = new Circle(radius);
+            IFigure triangle = new Triangle(Aside, Bside, Cside);
+
+            Console.WriteLine(circle.FindArea());
+            Console.WriteLine(triangle.FindArea());
+            Console.WriteLine($"The triangle is rectangular - {IsRectangular.Triangle(triangle)}");
+            
+        }
+    }
+}
